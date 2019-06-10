01b73eba99a856bcd14734f5934ea46900b2db7d
Alexandre Antonino Gonçalves Martinazzo <alexandremartinazzo@gmail.com>
2019-06-10 17:08:20 -0300

    django 2 adjustments

--- idp/urls.py
+++ idp/urls.py
@@ -1,15 +1,12 @@
-from django.conf.urls import (
-    include,
-    url,
-)
+from django.urls import include, path
 from django.contrib import admin
 from django.views.generic import TemplateView
 
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^accounts/', include('django.contrib.auth.urls')),
-    url(r'^idp/', include('djangosaml2idp.urls')),
-    url(r'^$', TemplateView.as_view(template_name="index.html")),
-    url(r'^iframe/', TemplateView.as_view(template_name="iframe.html")),
+    path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('idp/', include('djangosaml2idp.urls')),
+    path('', TemplateView.as_view(template_name="index.html")),
+    path('iframe/', TemplateView.as_view(template_name="iframe.html")),
 ]
